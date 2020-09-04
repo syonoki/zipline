7542ce4f7dda1487a222c41296fefba4f5305cbd
Injae.Lee <injae.lee@hanwha.com>
2020-09-04 14:16:58 +0900

    Revert "FIX: shiftdates의 return이 잘못된거 수정"
    This reverts commit 8d111ac23d4ec7685473aa606ad6693a042a1355.

--- zipline/pipeline/loaders/utils.py
+++ zipline/pipeline/loaders/utils.py
@@ -335,5 +335,4 @@ def shift_dates(dates, start_date, end_date, shift):
         else:
             raise ValueError("Query end %s not in calendar" % end_date)
 
-    targets = dates[start - shift:end - shift + 1] # +1 to be inclusive
-    return targets[0], targets[-1]
+    return dates[start - shift:end - shift + 1]  # +1 to be inclusive
